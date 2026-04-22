33cf934ef01599bd690e68170d1566b8915e2877
christinepayad <cpayad355@gmail.com>
2026-04-22 13:36:12 +0800

    Add files via upload

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Lavender;
+            panel1.ForeColor = Color.FromArgb(192, 192, 255);
+            panel1.Location = new Point(-17, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(541, 115);
+            panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(508, 567);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "                      ";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+    }
+}
